b70ab80379055babf9e6aa9cc2276849a787bdb2
Hideaki Takahashi <hideakitakahashi99@gmail.com>
2015-06-15 01:34:14 +0000

    design11

--- app/forms/customer/staff_member_search_form.rb
+++ app/forms/customer/staff_member_search_form.rb
@@ -7,10 +7,14 @@ class Customer::StaffMemberSearchForm
 		rel = Product
 		if category.present?
 			rel = rel.where(category: category)
+		else
+			rel = Product.all
 		end
 
 		if prefecture.present?
 			rel = rel.joins(:staff_addresses).where('staff_addresses.prefecture' => prefecture)
+		else
+			rel = rel.joins(:staff_addresses).all
 		end
 
 	end
